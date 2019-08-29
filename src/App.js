a32a9b7e22abeb3d5746552e5aaca741217b5a63
Guy Shpigler <guy.shpigler@Guys-MacBook-Pro.local>
2019-08-29 12:35:54 +0300

    dockerignore added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          you have reached the end of the development flow!
+          you have reached the end of the development flow!!!
         </p>
         <a
           className="App-link"
