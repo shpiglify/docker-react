e386b7b84b1dbb6655ff4b66c16c1586e502a6f4
Guy Shpigler <guy.shpigler@Guys-MacBook-Pro.local>
2019-08-29 11:51:28 +0300

    docker files added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          you have reached the end of the development flow!
         </p>
         <a
           className="App-link"
